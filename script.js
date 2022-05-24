d921833e6269c7915a28b8a2254910ac31c405c5
yugmade13 <yugmade@gmail.com>
2022-05-24 11:32:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('click', event => {
+    const isDropdownButton = event.target.matches('[data-dropdown-button]');
+
+    if(!isDropdownButton && event.target.closest('[data-dropdown]') != null) return;
+
+    let currentDropdown;
+    if(isDropdownButton) {
+        currentDropdown = event.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+        if(dropdown === currentDropdown) return;
+        
+        dropdown.classList.remove('active');
+    });
+});
